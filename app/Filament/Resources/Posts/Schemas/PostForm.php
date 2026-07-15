e06e87c3e3237861e035af476f90772dec981678
RakibIT <rakib.itsolution@gmail.com>
2026-07-15 08:33:10 +0600

    R15->post image not work

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -11,7 +11,12 @@ use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+
+
 
 class PostForm
 {
@@ -19,17 +24,43 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make('title'),
-                TextInput::make('slug'),
-                Select::make('category_id')
-                    ->label('Category')
-                    ->options(Category::all()->pluck('name', 'id')),
-                ColorPicker::make('color'),
-                MarkdownEditor::make('body'),
-                FileUpload::make('image')->disk('public')->directory('posts'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                DatePicker::make('published_at'),
-            ]);
+                Section::make('Post Details')
+                    ->description('Fill all the fields')
+                    ->icon(Heroicon::RocketLaunch)
+                    ->schema([
+                        Group::make()
+                            ->schema([
+                                TextInput::make('title')->rules(['required', 'string', 'max:255']),
+                                TextInput::make('slug'),
+                                Select::make('category_id')
+                                    ->label('Category')
+                                    ->options(Category::all()->pluck('name', 'id')),
+                                ColorPicker::make('color'),
+
+                            ])->columns(2),
+
+                        MarkdownEditor::make('body'),
+                    ])->columnSpan(2),
+
+                Group::make()
+                    ->schema([
+                        Section::make('Post Media')
+                            ->description('Upload media files')
+                            ->icon(Heroicon::Photo)
+                            ->schema([
+                                FileUpload::make('image')->disk('public'),
+                                TagsInput::make('tags'),
+                            ]),
+                        Section::make('Post Settings')
+                            ->description('Configure post settings')
+                            ->icon(Heroicon::Cog)
+                            ->schema([
+                                Checkbox::make('published'),
+                                DatePicker::make('published_at'),
+                            ]),
+
+                    ])->columnSpan(1),
+
+            ])->columns(3);
     }
 }
